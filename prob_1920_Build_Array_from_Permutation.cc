ceb93708bbb0ebe00e0700999ca75cb406fb7880
Madhav Sharma <53596634+maddyhunk@users.noreply.github.com>
2024-04-23 00:29:44 +0530

    Add files via upload
    1920. Build Array from Permutation
    
    Solution : O(n)

---
+++ prob_1920_Build_Array_from_Permutation.cc
@@ -0,0 +1,69 @@
+/*
+Given a zero-based permutation nums (0-indexed), build an array ans of the same length where
+  ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+
+A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+
+ 
+
+Example 1:
+
+Input: nums = [0,2,1,5,3,4]
+Output: [0,1,2,4,5,3]
+Explanation: The array ans is built as follows: 
+ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
+    = [nums[0], nums[2], nums[1], nums[5], nums[3], nums[4]]
+    = [0,1,2,4,5,3]
+*/
+
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    int n = 0;
+    int *nums;
+    int *ans;
+
+    std::cout << " Enter val of n : ";
+    std::cin>>n;
+    nums = new int [n];
+    ans = new int [n];
+    for (int i = 0; i < n; i++) {
+        std::cin>>nums[i];
+    }
+    printf("\n Printing nums : ");
+    for (int i = 0; i < n; i++) {
+      std::cout << nums[i];
+      if ( i < n - 1) {
+        printf(", ");
+      }
+    }
+
+    // core code logic
+    for (int i = 0; i < n; i++) {
+        int a = nums[i];
+        if (a >=0 && a < n) {
+            ans[i] = nums[a];
+        }
+    }
+
+    printf("\n Printing nums : ");
+    for (int i = 0; i < n; i++) {
+      std::cout << nums[i];
+      if ( i < n - 1) {
+        printf(", ");
+      }
+    }
+    printf("\n Printing ans : ");
+    for (int i = 0; i < n; i++) {
+        std::cout << ans[i];
+        if (i < (n-1)) {
+          printf(", ");
+        }
+    }
+
+    delete [] nums;
+    delete [] ans;
+
+    return 0;
+}
